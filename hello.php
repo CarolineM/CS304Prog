67ca8055bc88c6e6930227083a74b87e0a05a355
Caroline McQuatt <cmcquatt@gmail.com>
2012-03-13 19:21:15 -0700

    first commit

---
+++ hello.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php print "Hello World"; ?> 
+        <?php echo '<p>Hello World</p>'; ?> 
+    </body>
+</html>
+
