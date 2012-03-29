41088e82c61328be7ec3d45d42970723266235e4
Benjamin Shieh <shiehben@gmail.com>
2012-03-29 16:43:15 -0700

    Interface for add_course and search

--- add_course.php
+++ add_course.php
@@ -1,11 +1,41 @@
-
-<?php include("header.php"); ?>
-
-    <div class="container">
-
-      <h1>Add a course to the DB</h1>
-      <p>Can be accessed from the search view</p>
-
+	<div class="container">
+		<h1>Add a course to the DB</h1>
+		<p>Can be accessed from the search view</p>
+		<form>
+		<table>
+		<tbody>
+		<tr>
+			<td>Course Name:</td>
+			<td>
+			<input type="text" /></td>
+		</tr>
+		<tr>
+			<td>Course Department:</td>
+			<td>
+			<input type="text" /></td>
+		</tr>
+		<tr>
+			<td>Institution:</td>
+			<td>
+			<input type="text" /></td>
+		</tr>
+		<tr>
+			<td>Term:</td>
+			<td>
+			<input type="text" /></td>
+		</tr>
+		<tr>
+			<td>Year:</td>
+			<td>
+			<input type="number" /></td>
+		</tr>
+		<tr>
+			<td>
+			<input id="subbut" type="button" value="Submit" /></td>
+		</tr>
+		</tbody>
+		</table>
+		</form>
     </div> <!-- /container -->
 
 <?php include("footer.php"); ?>
