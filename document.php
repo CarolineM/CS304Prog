d0317cd06ff94cb1298ca4934a033d8bbdf669b6
davidslater250 <davidslater250@gmail.com>
2012-03-29 16:00:23 -0700

    commit

--- document.php
+++ document.php
@@ -22,12 +22,13 @@
 		<option value="Option4">doc4</option>
 		</select>
 	</p>
+	<input type="button" onclick="" value="Load Document"/>
 	
 	<iframe src="http://docs.google.com/gview?url=http://www.ugrad.cs.ubc.ca/~cs304/2011W2/notes/Chapter1-intro-2up.pdf" style="width:1000px; height:600px;" frameborder="0"></iframe>
 	
 	<textarea name="comment" cols="80">Insert document comments here...</textarea> </br>
 	
-	<input type="button" onclick="<?php echo 'wurd'?>" value="Submit Comment"/>
+	<input type="button" onclick="" value="Submit Comment"/>
 	
 	<br> haxorGuy - March 26, 2012 8:22 - These are old notes</br>
 	<br> haxorGirl - March 26, 2012 8:35 - No they aren't, I just posted them.</br>
@@ -36,7 +37,7 @@
 	SUBMIT COMMENT
 	
 	TIMESTAMP		
-		var $timestamp = date(DATE_RFC822); example: 2005-08-15T15:52:01+00:00
+		var $timestamp = date(DATE_RFC822);
 		date_format($timestamp, 'yyyy/mm/dd:hh:mi:ss'); 
 		
 	SUBMIT COMMENT QUERY
@@ -55,6 +56,10 @@
 		
 	RETRIEVE DOCUMENTS IN A COURSE(from list)
 		select document_id from document where course_num = (var from course number table);
+		
+	RETRIEVE DOCUMENT GIVEN DOC NAME (we have to add document name)
+		select document_id from document where doc_name = (doc name from table);
+		this needs more for unique
 	  -->
 	  
 
