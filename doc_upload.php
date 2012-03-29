dbec9130a2ed0ea9d0e3601eb6689c238cdc1c0b
Ben Jeffery <bencjeffery@gmail.com>
2012-03-29 16:25:40 -0700

    Basic Doc upload UI

--- doc_upload.php
+++ doc_upload.php
@@ -1,11 +1,21 @@
 
-<?php include("header.php"); ?>
+
 
     <div class="container">
-
-      <h1>Upload a pdf</h1>
-      <p>Should be linked from a course.</p>
-
+		<?php session_start(); ?>
+      <h1>Create a document!</h1>
+      <p>We do not host documents locally: Please input the URL of the document you wish to display.</p>
+	  <label for="courseSelect">Select a course:</label>
+		<select name="courseSelect">
+			<option value="course1">Course 1</option>
+			<option value="course2">Course 2</option>
+			<option value="course2">Course 2</option>
+		</select>
+		<br />
+		<label for="docURL">Input the document URL:</label>
+		<input type="text"></input>
+		<br />
+		<input type="button" value="Submit" />
+	  
     </div> <!-- /container -->
 
-<?php include("footer.php"); ?>
