2f4db94f3d55a4dd13c0f0e3ad5d7be5a921dd24
Caroline McQuatt <cmcquatt@gmail.com>
2012-03-31 16:54:54 -0700

    settings page and password change work

--- settings.php
+++ settings.php
@@ -5,14 +5,14 @@
     <div class="container">
 
       <h1>User Information</h1>
-      <p>All user specific information and queries</p>
+      <hr/>
       <?php
       
       if ($db_conn=OCILogon("ora_p1t7", "a36959104", "ug")){
 
         //user documents query
         $email = $_SESSION['email'];
-        $cmdstr = "select document_id, institution, dept, course_num, document_time from document where email= '$email'";
+        $cmdstr = "select document_id,  document_name, institution, dept, course_num, document_time from document where email= '$email'";
    
         $parsed = OCIParse($db_conn, $cmdstr);
         if (!$parsed){
@@ -50,29 +50,67 @@
         
     echo "<tr>
           <td bgcolor=\"#COCOCO\">" . $row[0] . "</td>
-          <td bgcolor=\"#COCOCO\">" . "PLACEHOLDER" . "</td>
           <td bgcolor=\"#COCOCO\">" . $row[1] . "</td>
           <td bgcolor=\"#COCOCO\">" . $row[2] . "</td>
           <td bgcolor=\"#COCOCO\">" . $row[3] . "</td>
           <td bgcolor=\"#COCOCO\">" . $row[4] . "</td>
           <td bgcolor=\"#COCOCO\">" . $row[5] . "</td>
+          <td bgcolor=\"#COCOCO\">" . $row[6] . "</td>
         </tr>";
     while ($row = OCI_Fetch_Array($parsed, OCI_NUM)) {
     echo "<tr>
           <td bgcolor=\"#COCOCO\">" . $row[0] . "</td>
-          <td bgcolor=\"#COCOCO\">" . "PLACEHOLDER" . "</td>
           <td bgcolor=\"#COCOCO\">" . $row[1] . "</td>
           <td bgcolor=\"#COCOCO\">" . $row[2] . "</td>
           <td bgcolor=\"#COCOCO\">" . $row[3] . "</td>
           <td bgcolor=\"#COCOCO\">" . $row[4] . "</td>
           <td bgcolor=\"#COCOCO\">" . $row[5] . "</td>
+          <td bgcolor=\"#COCOCO\">" . $row[6] . "</td>
         </tr>";
     }
     echo "</table>";
+   
+          //user comments
+          $cmdstr = "select comment_id, text, comment_time from ns_comment where email = '$email'";
+   
+          $parsed = OCIParse($db_conn, $cmdstr);
+          if (!$parsed){
+            $e = OCIError($db_conn);  
+            echo htmlentities($e['message']); 
+            exit;
+          }
+
+        $r=OCIExecute($parsed, OCI_DEFAULT); 
+        if (!$r){
+          $e = oci_error($parsed); 
+          echo htmlentities($e['message']);
+          exit;
+        }
+   
+    //echo couse table
+       echo '<br/>';
+       echo '<h3>Your Comments</h3>';
+       echo "<table width=\"100%\" border=\"1\">";
+       echo "<tr>
+           <th>ID</th>
+           <th>Text</th>
+           <th>Time</th>
+           <tr>";
+       while ($row = OCI_Fetch_Array($parsed, OCI_NUM)) {
+       echo "<tr>
+            <td bgcolor=\"#COCOCO\">" . $row[0] . "</td>
+            <td bgcolor=\"#COCOCO\">" . $row[1] . "</td>
+            <td bgcolor=\"#COCOCO\">" . $row[2] . "</td>
+          </tr>";
+      }
+      echo "</table>";
    }
-           //user comments
-        if ($areDocuments) {
-          $cmdstr = "select comment_id, comment_time, text from ns_comment where ns_comment.email = '$email'";
+
+       
+        if ($areDocuments || $areComments) {
+                  
+           //user course query interation query (two JOINs with union)
+          $cmdstr = "select course_is_in.course_num, course_is_in.course_name from course_is_in, document where document.email = '$email' and course_is_in.course_num = document.course_num union select course_is_in.course_num, course_is_in.course_name from course_is_in, ns_comment where ns_comment.email = '$email' and course_is_in.course_num = ns_comment.course_num";
    
           $parsed = OCIParse($db_conn, $cmdstr);
           if (!$parsed){
@@ -99,58 +137,34 @@
        while ($row = OCI_Fetch_Array($parsed, OCI_NUM)) {
        echo "<tr>
             <td bgcolor=\"#COCOCO\">" . $row[0] . "</td>
-            <td bgcolor=\"#COCOCO\">" . "PLACEHOLDER" . "</td>
+            <td bgcolor=\"#COCOCO\">" . $row[1] . "</td>
           </tr>";
       }
       echo "</table>";
    }
-   
-        //user course query interation query (two JOINs with union)
-        if ($areDocuments) {
-          $cmdstr = "select course_is_in.course_num from course_is_in, document where document.email = '$email' and course_is_in.course_num = document.course_num union select course_is_in.course_num from course_is_in, ns_comment where ns_comment.email = '$email' and course_is_in.course_num = ns_comment.course_num";
-   
-          $parsed = OCIParse($db_conn, $cmdstr);
-          if (!$parsed){
-            $e = OCIError($db_conn);  
-            echo htmlentities($e['message']); 
-            exit;
-          }
-
-        $r=OCIExecute($parsed, OCI_DEFAULT); 
-        if (!$r){
-          $e = oci_error($parsed); 
-          echo htmlentities($e['message']);
-          exit;
-        }
-   
-    //echo couse table
-       echo '<br/>';
-       echo '<h3>Courses You Have Added Content To</h3>';
-       echo "<table width=\"100%\" border=\"1\">";
-       echo "<tr>
-           <th>Course Number</th>
-           <th>Name</th>
-           <tr>";
-       while ($row = OCI_Fetch_Array($parsed, OCI_NUM)) {
-       echo "<tr>
-            <td bgcolor=\"#COCOCO\">" . $row[0] . "</td>
-            <td bgcolor=\"#COCOCO\">" . "PLACEHOLDER" . "</td>
-          </tr>";
-      }
-      echo "</table>";
-   }
-
-   
    //logoff
     OCILogoff($db_conn);
   }
-  //error condition
+  //error on connection
   else {
     $e = OCIError();  
     echo htmlentities($e['message']);
    }
 
 ?>
+<hr/>
+<h1>Change Password</h1>
+    <form method="POST" action="change_password.php">
+         <p><h3>Old Password</h3><input type="password" name="old_password" size="18"/></p>
+         <p><h3>New Password</h3><input type="password" name="new_password" size="18"/></p>
+         <p><input class="btn-large" type="submit" value="submit" name="insertsubmit"></p>
+    </form>
+  <?php
+  if (isset($_SESSION['pw_change_result'])) {
+    echo "<b>" . $_SESSION['pw_change_result'] . "</b>";
+  }
+  ?>
+
 
 
     </div> <!-- /container -->
