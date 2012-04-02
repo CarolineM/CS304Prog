e1d0efbe3e265654f57a76e8c2742b12da0bff2c
Benjamin Shieh <shiehben@gmail.com>
2012-04-01 19:24:23 -0700

    assertions in search

--- search_sql.php
+++ search_sql.php
@@ -1,17 +1,56 @@
 <?php include("header.php"); ?>
 <div class="container">
 <?php
-
+ session_start();
 $cname=$_POST['cname'];
 $cnumb=$_POST['cnumb'];
 $cdept=$_POST['cdept'];
 $cinst=$_POST['cinst'];
 $cterm=$_POST['cterm'];
 $cyear=$_POST['cyear'];
-$cdocs=$_POST['cdocs'];
 $db_conn = OCILogon("ora_n2f7", "a46785093", "ug");
 
 //check variables valid
+   if (is_numeric($cname)) {
+        $cname = strval($cname);
+   }
+      if (is_numeric($cdept)) {
+        $cdept = strval($cdept);
+   }
+      if (is_numeric($cinst)) {
+        $cinst = strval($cinst);
+   }
+      if (is_numeric($cterm)) {
+        $cterm = strval($cterm);
+   }
+      if (is_numeric($cyear)) {
+        $cyear = strval($cyear);
+   }
+   
+   if (strlen($cnumb) >= 4) {
+	$_SESSION['insert_course_err'] = "Course number must have 3 digits or less";
+	header('Location:search.php');
+	exit;
+   }
+      if (strlen($cterm) >= 5) {
+	$_SESSION['insert_course_err'] = "Term must have 4 digits or less";
+	header('Location:search.php');
+	exit;
+      }
+      
+        if (strlen($cyear) >= 5) {
+	$_SESSION['insert_course_err'] = "Year must have 4 digits or less";
+	header('Location:search.php');
+	exit;
+      }
+      
+        if (strlen($cdept) >= 5) {
+	$_SESSION['insert_course_err'] = "Department must have 4 digits or less";
+	header('Location:search.php');
+	exit;
+      }
+
+//Build query
 $selectquery = "SELECT * FROM course_is_in WHERE";
 if (!empty($cnumb)) {
 	$selectquery .= " course_num=$cnumb AND";
@@ -39,49 +78,51 @@ echo "Your query: " . $selectquery . "<br>";
 if ($db_conn)
 {
 	//queries
-	if ($cdocs == 1) {
-		//join query course_is_in and documents
+	//parse query
+	$success = True;
+	$parse = OCIParse($db_conn, $selectquery);
+	if (!$parse) {
+		echo "<br>Cannot parse the following command: " . $selectquery . "<br>";
+		$e = OCI_Error($db_conn); // For OCIParse errors pass the       
+		// connection handle
+		$_SESSION['insert_course_err'] = htmlentities($e['message']);
+		$success = False;
 	} else {
-		//parse query
-		$parse = OCIParse($db_conn, $selectquery);
-		if (!$parse) {
-			echo "<br>Cannot parse the following command: " . $selectquery . "<br>";
-			$e = OCI_Error($db_conn); // For OCIParse errors pass the       
-			// connection handle
-			echo htmlentities($e['message']);
+	//execute query
+		$r = OCIExecute($parse, OCI_DEFAULT);
+		if (!$r) {
+			echo "<br>Cannot execute the following command: " . $selectquery . "<br>";
+			$e = oci_error($parse); // For OCIExecute errors pass the statementhandle
+			$_SESSION['insert_course_err'] = htmlentities($e['message']);
+			$success = False;
 		} else {
-		//execute query
-			$r = OCIExecute($parse, OCI_DEFAULT);
-			if (!$r) {
-				echo "<br>Cannot execute the following command: " . $selectquery . "<br>";
-				$e = oci_error($parse); // For OCIExecute errors pass the statementhandle
-				echo htmlentities($e['message']);
-			} else {
-		//return query
-				echo "<h3>Results</h3>";
-				echo "<table width=\"100%\" border=\"1\">";
+	//return query
+			echo "<h3>Results</h3>";
+			echo "<table width=\"100%\" border=\"1\">";
+			echo "<tr>
+					<th>Course Number</th>
+					<th>Course Name</th>
+					<th>Department</th>
+					<th>Institution</th>
+					<th>Semester</th>
+					<th>Year</th>
+				</tr>";
+			while($row = oci_fetch_array($parse, OCI_NUM)) {
 				echo "<tr>
-						<th>Course Number</th>
-						<th>Course Name</th>
-						<th>Department</th>
-						<th>Institution</th>
-						<th>Semester</th>
-						<th>Year</th>
-					</tr>";
-				while($row = oci_fetch_array($parse, OCI_NUM)) {
-					echo "<tr>
-							  <td bgcolor=\"#COCOCO\">" . $row[0] . "</td>
-							  <td bgcolor=\"#COCOCO\">" . $row[1] . "</td>
-							  <td bgcolor=\"#COCOCO\">" . $row[2] . "</td>
-							  <td bgcolor=\"#COCOCO\">" . $row[3] . "</td>
-							  <td bgcolor=\"#COCOCO\">" . $row[4] . "</td>
-							  <td bgcolor=\"#COCOCO\">" . $row[5] . "</td>
-							</tr>";
-				}
-				echo "</table>";
+						  <td bgcolor=\"#COCOCO\">" . $row[0] . "</td>
+						  <td bgcolor=\"#COCOCO\">" . $row[1] . "</td>
+						  <td bgcolor=\"#COCOCO\">" . $row[2] . "</td>
+						  <td bgcolor=\"#COCOCO\">" . $row[3] . "</td>
+						  <td bgcolor=\"#COCOCO\">" . $row[4] . "</td>
+						  <td bgcolor=\"#COCOCO\">" . $row[5] . "</td>
+						</tr>";
 			}
+			echo "</table>";
 		}
 	}
+}
+if(!$success) {
+	header('Location:search.php');
 }
 
 ?>
